1b723c7a5d49d4a11f1b31c145c30f05d7b46242
AsteroidDown <jasper@stahmer.ca>
2024-09-12 22:09:44 -0400

    Trying to get relative paths

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,18 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
+
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+
+          alias: {
+            "@": "./",
+          },
+        },
+      ],
+    ],
   };
 };
