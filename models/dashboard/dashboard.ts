ec16aa3cd9b7853e7424219189956b61c7366601
AsteroidDown <jasper@stahmer.ca>
2024-09-29 22:51:43 -0400

    Added graph and dashboard section ids

--- models/dashboard/dashboard.ts
+++ models/dashboard/dashboard.ts
@@ -5,12 +5,15 @@ export interface Dashboard {
 }
 
 export interface DashboardSection {
+  id: string;
   title: string;
   graphs: DashboardGraph[];
 }
 
 export interface DashboardGraph {
+  id: string;
   title: string;
   type: string;
+  stacked: boolean;
   filters: CardFilters;
 }
