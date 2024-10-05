cb89b839bc2dc1370a648e322f7bedd7b8f90886
AsteroidDown <jasper@stahmer.ca>
2024-10-05 00:48:10 -0400

    Readded rarity filter

--- components/ui/filters/filter-types/rarity-filter.tsx
+++ components/ui/filters/filter-types/rarity-filter.tsx
@@ -99,7 +99,9 @@ export default function RarityFilter({
         expanded={expanded}
         setExpanded={setExpanded}
         className={`!max-w-[360px] px-4 py-2 border-2 border-primary-300 bg-dark-200 rounded-2xl shadow-lg`}
-      ></Dropdown>
+      >
+        {rarityFiltersList}
+      </Dropdown>
     </View>
   );
 }
