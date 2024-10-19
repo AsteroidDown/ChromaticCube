31c5650caa75b29a26c042b2519a948c19e62829
AsteroidDown <jasper@stahmer.ca>
2024-10-19 17:51:52 -0400

    Added details to chart cell cards

--- components/chart/chart-cell.tsx
+++ components/chart/chart-cell.tsx
@@ -7,10 +7,10 @@ import { MTGRarity } from "@/constants/mtg/mtg-rarity";
 import { titleCase } from "@/functions/text-manipulation";
 import { Card } from "@/models/card/card";
 import { CardsSortedByColor } from "@/models/sorted-cards/sorted-cards";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faShop } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "expo-router";
 import React from "react";
-import { Pressable } from "react-native";
+import { Linking, Pressable, View } from "react-native";
 import Modal from "../ui/modal/modal";
 import Text from "../ui/text/text";
 import { ChartType } from "./chart";
@@ -83,9 +83,45 @@ export default function ChartCell({
           {title + ` (${cards.length})`}
         </Text>
 
-        <Box className="flex flex-row flex-wrap gap-2 min-h-[350px] max-h-[80vh] min-w-[228px] w-fit max-w-[938px] !px-0 overflow-x-auto">
+        <Box className="flex flex-row flex-wrap gap-2 min-h-[350px] max-h-[80vh] w-fit min-w-[228px] max-w-[1000px] !px-0 overflow-x-auto">
           {cards?.map((card: Card, index: number) => (
-            <CardImage card={card} key={card.id + index} />
+            <Box
+              key={card.id + index}
+              className="flex gap-1 !bg-background-100 !p-2 max-w-[244px]"
+            >
+              <View className="flex flex-row gap-2 px-2">
+                <Text thickness="bold">{card.count}</Text>
+                <Text truncate thickness="bold">
+                  {card.name}
+                </Text>
+              </View>
+
+              <CardImage card={card} />
+
+              <View className="flex flex-row gap-2 px-2 pb-1">
+                <Button
+                  size="xs"
+                  action="info"
+                  className="flex-1"
+                  icon={faShop}
+                  text={`$${card.prices?.usd}`}
+                  onClick={async () =>
+                    await Linking.openURL(card.priceUris.tcgplayer)
+                  }
+                />
+
+                <Button
+                  size="xs"
+                  action="info"
+                  className="flex-1"
+                  icon={faShop}
+                  text={`€${card.prices?.eur}`}
+                  onClick={async () =>
+                    await Linking.openURL(card.priceUris.cardmarket)
+                  }
+                />
+              </View>
+            </Box>
           ))}
 
           {!cards?.length && (
