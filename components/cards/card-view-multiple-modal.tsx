2d21bba84137fdc0eda503eb1fac15a8ffee3732
AsteroidDown <jasper@stahmer.ca>
2024-10-20 12:39:46 -0400

    Created card view multiple component

---
+++ components/cards/card-view-multiple-modal.tsx
@@ -0,0 +1,121 @@
+import { MTGColor } from "@/constants/mtg/mtg-colors";
+import { MTGRarity } from "@/constants/mtg/mtg-rarity";
+import { currency, titleCase } from "@/functions/text-manipulation";
+import { Card } from "@/models/card/card";
+import { faPlus, faShop } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "expo-router";
+import React from "react";
+import { Linking, View } from "react-native";
+import { ChartType } from "../chart/chart";
+import Box from "../ui/box/box";
+import BoxHeader from "../ui/box/box-header";
+import Button from "../ui/button/button";
+import Modal from "../ui/modal/modal";
+import Placeholder from "../ui/placeholder/placeholder";
+import Text from "../ui/text/text";
+import CardImage from "./card-image";
+
+export interface CardViewMultipleModalProps {
+  cards: Card[];
+  color: MTGColor;
+  cost?: number;
+  rarity?: MTGRarity;
+  type?: string;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function CardViewMultipleModal({
+  cards,
+  color,
+  cost,
+  rarity,
+  type,
+  open,
+  setOpen,
+}: CardViewMultipleModalProps) {
+  const cellType: ChartType = cost ? "cost" : rarity ? "rarity" : "type";
+
+  const title = `${titleCase(color)} ${
+    cellType === "cost"
+      ? cost
+        ? cost === 7
+          ? "7+ Cost"
+          : cost + " Cost"
+        : ""
+      : cellType === "rarity"
+      ? rarity
+        ? titleCase(rarity)
+        : ""
+      : titleCase(type)
+  } Cards`;
+
+  const subtitle = `${cards.reduce(
+    (acc, card) => acc + card.count,
+    0
+  )} Cards | Total Value: ${currency(
+    cards.reduce(
+      (acc, card) => acc + (card.prices?.usd || card.prices?.eur || 0),
+      0
+    )
+  )}`;
+
+  return (
+    <Modal open={open} setOpen={setOpen}>
+      <BoxHeader title={title} subtitle={subtitle} />
+
+      <Box className="flex flex-row flex-wrap gap-2 min-h-[350px] max-h-[80vh] w-fit min-w-[228px] max-w-[1000px] !px-0 overflow-x-auto">
+        {cards?.map((card: Card, index: number) => (
+          <Box
+            key={card.id + index}
+            className="flex gap-1 !bg-background-100 !p-2 max-w-[244px]"
+          >
+            <View className="flex flex-row gap-2 px-2">
+              <Text thickness="bold">{card.count}</Text>
+              <Text truncate thickness="bold">
+                {card.name}
+              </Text>
+            </View>
+
+            <CardImage card={card} />
+
+            <View className="flex flex-row gap-2 px-2 pb-1">
+              <Button
+                size="xs"
+                action="info"
+                className="flex-1"
+                icon={faShop}
+                text={`$${card.prices?.usd}`}
+                onClick={async () =>
+                  await Linking.openURL(card.priceUris.tcgplayer)
+                }
+              />
+
+              <Button
+                size="xs"
+                action="info"
+                className="flex-1"
+                icon={faShop}
+                text={`€${card.prices?.eur}`}
+                onClick={async () =>
+                  await Linking.openURL(card.priceUris.cardmarket)
+                }
+              />
+            </View>
+          </Box>
+        ))}
+
+        {!cards?.length && (
+          <Placeholder
+            title="No Cards Found!"
+            subtitle={`Add some ${title.toLowerCase()} and they'll show up here!`}
+          >
+            <Link href="./(tabs)/main-board">
+              <Button icon={faPlus} text="Add Cards"></Button>
+            </Link>
+          </Placeholder>
+        )}
+      </Box>
+    </Modal>
+  );
+}
--- components/chart/chart-cell.tsx
+++ components/chart/chart-cell.tsx
@@ -1,19 +1,11 @@
-import CardImage from "@/components/cards/card-image";
-import Box from "@/components/ui/box/box";
-import Button from "@/components/ui/button/button";
-import Placeholder from "@/components/ui/placeholder/placeholder";
 import { MTGColor } from "@/constants/mtg/mtg-colors";
 import { MTGRarity } from "@/constants/mtg/mtg-rarity";
-import { titleCase } from "@/functions/text-manipulation";
 import { Card } from "@/models/card/card";
 import { CardsSortedByColor } from "@/models/sorted-cards/sorted-cards";
-import { faPlus, faShop } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "expo-router";
 import React from "react";
-import { Linking, Pressable, View } from "react-native";
-import Modal from "../ui/modal/modal";
+import { Pressable } from "react-native";
+import CardViewMultipleModal from "../cards/card-view-multiple-modal";
 import Text from "../ui/text/text";
-import { ChartType } from "./chart";
 
 interface ChartCellProps {
   sortedCards: CardsSortedByColor;
@@ -36,8 +28,6 @@ export default function ChartCell({
 }: ChartCellProps) {
   const [cardPreviewModalOpen, setCardPreviewModalOpen] = React.useState(false);
 
-  const cellType: ChartType = cost ? "cost" : rarity ? "rarity" : "type";
-
   const borderClasses =
     (!hideRightBorder ? (double ? "border-r-2" : "border-r") : "") +
     " border-t border-background-300";
@@ -49,20 +39,6 @@ export default function ChartCell({
       Object.keys(sortedCards).findIndex((key) => key === color) || 0
     ];
 
-  const title = `${titleCase(color)} ${
-    cellType === "cost"
-      ? cost
-        ? cost === 7
-          ? "7+ Cost"
-          : cost + " Cost"
-        : ""
-      : cellType === "rarity"
-      ? rarity
-        ? titleCase(rarity)
-        : ""
-      : titleCase(type)
-  } Cards`;
-
   return (
     <>
       <Pressable
@@ -72,70 +48,19 @@ export default function ChartCell({
         onPress={() => setCardPreviewModalOpen(true)}
       >
         <Text>
-          {Object.values(sortedCards)[
-            Object.keys(sortedCards).findIndex((key) => key === color) || 0
-          ]?.reduce((acc: number, card: Card) => acc + card.count, 0)}
+          {cards?.reduce((acc: number, card: Card) => acc + card.count, 0)}
         </Text>
       </Pressable>
 
-      <Modal open={cardPreviewModalOpen} setOpen={setCardPreviewModalOpen}>
-        <Text size="lg" thickness="bold">
-          {title + ` (${cards.length})`}
-        </Text>
-
-        <Box className="flex flex-row flex-wrap gap-2 min-h-[350px] max-h-[80vh] w-fit min-w-[228px] max-w-[1000px] !px-0 overflow-x-auto">
-          {cards?.map((card: Card, index: number) => (
-            <Box
-              key={card.id + index}
-              className="flex gap-1 !bg-background-100 !p-2 max-w-[244px]"
-            >
-              <View className="flex flex-row gap-2 px-2">
-                <Text thickness="bold">{card.count}</Text>
-                <Text truncate thickness="bold">
-                  {card.name}
-                </Text>
-              </View>
-
-              <CardImage card={card} />
-
-              <View className="flex flex-row gap-2 px-2 pb-1">
-                <Button
-                  size="xs"
-                  action="info"
-                  className="flex-1"
-                  icon={faShop}
-                  text={`$${card.prices?.usd}`}
-                  onClick={async () =>
-                    await Linking.openURL(card.priceUris.tcgplayer)
-                  }
-                />
-
-                <Button
-                  size="xs"
-                  action="info"
-                  className="flex-1"
-                  icon={faShop}
-                  text={`€${card.prices?.eur}`}
-                  onClick={async () =>
-                    await Linking.openURL(card.priceUris.cardmarket)
-                  }
-                />
-              </View>
-            </Box>
-          ))}
-
-          {!cards?.length && (
-            <Placeholder
-              title="No Cards Found!"
-              subtitle={`Add some ${title.toLowerCase()} and they'll show up here!`}
-            >
-              <Link href="./(tabs)/main-board">
-                <Button icon={faPlus} text="Add Cards"></Button>
-              </Link>
-            </Placeholder>
-          )}
-        </Box>
-      </Modal>
+      <CardViewMultipleModal
+        open={cardPreviewModalOpen}
+        setOpen={setCardPreviewModalOpen}
+        cards={cards}
+        color={color}
+        cost={cost}
+        rarity={rarity}
+        type={type}
+      />
     </>
   );
 }
